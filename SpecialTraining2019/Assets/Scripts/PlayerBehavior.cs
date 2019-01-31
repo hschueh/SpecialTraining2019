908c51fde6f46ab17bf0f2065564b5d814427077
hschueh <chueh044@gmail.com>
2019-02-01 00:44:21 +0800

    Rename scene. Add scripts to let player movable.

---
+++ SpecialTraining2019/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    Rigidbody2D rb;
+    //Vector2 speed = new Vector2(0, 0);
+    const float speedValue = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPos = rb.position;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            newPos.y += speedValue;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            newPos.y -= speedValue;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPos.x -= speedValue;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            newPos.x += speedValue;
+        }
+        rb.MovePosition(newPos);
+    }
+}
