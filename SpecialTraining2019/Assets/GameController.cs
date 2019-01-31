31b93266446170215651bf637c0676b2f5513d8a
Lee Haw <lee790909@gmail.com>
2019-02-01 01:15:27 +0800

    add score text

---
+++ SpecialTraining2019/Assets/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    public Text mainText;
+
+    private bool gameStart;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        gameStart = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            gameStart = true;
+        }
+
+
+        if (gameStart == false)
+        {
+            return;
+        }
+
+        score++;
+
+        mainText.text = "Score: " + score.ToString();
+    }
+}
