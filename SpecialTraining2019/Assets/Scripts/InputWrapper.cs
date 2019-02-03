1dc2f85007f3fc381aea9bd3a662b2d012440e34
hschueh <chueh044@gmail.com>
2019-02-03 13:51:16 +0800

    Implement multiple kinds of touch control. Need to configure in code though

--- SpecialTraining2019/Assets/Scripts/InputWrapper.cs
+++ SpecialTraining2019/Assets/Scripts/InputWrapper.cs
@@ -8,13 +8,15 @@ public class InputWrapper : MonoBehaviour
     public static InputWrapper instance;
 
     public const int TYPE_DESKTOP = 0;
-    public const int TYPE_MOBILE = 1;
+    public const int TYPE_MOBILE_FIX_ON_TOUCH = 1;
+    public const int TYPE_MOBILE_FIXED_PIVOT = 2;
+    public const int TYPE_MOBILE_FLOATING_PIVOT = 3;
 
     public int type = -1;
 
     Vector2 movement = new Vector2(float.NaN, float.NaN);
     Vector2 nanVec2 = new Vector2(float.NaN, float.NaN);
-    Vector2 pivot;
+    Vector2 pivot = new Vector2(0.0f, -2.0f);
     GameObject joystickBase;
     GameObject stick;
 
@@ -39,20 +41,31 @@ public class InputWrapper : MonoBehaviour
     void Start()
     {
         joystickBase = GameObject.Find("JoystickBase");
-        joystickBase.GetComponent<SpriteRenderer>().enabled = false;
         stick = GameObject.Find("Joystick");
-        stick.GetComponent<SpriteRenderer>().enabled = false;
 
         switch (Application.platform)
         {
             case RuntimePlatform.Android:
             case RuntimePlatform.IPhonePlayer:
-                type = TYPE_MOBILE;
+                type = TYPE_MOBILE_FIX_ON_TOUCH;
                 break;
             default:
                 type = TYPE_DESKTOP;
                 break;
 
+        }
+
+        switch (type)
+        {
+            case TYPE_MOBILE_FIXED_PIVOT:
+                transform.position = new Vector3(pivot.x, pivot.y, 0);
+                break;
+            case TYPE_MOBILE_FLOATING_PIVOT:
+            case TYPE_MOBILE_FIX_ON_TOUCH:
+            case TYPE_DESKTOP:
+                stick.GetComponent<SpriteRenderer>().enabled = false;
+                joystickBase.GetComponent<SpriteRenderer>().enabled = false;
+                break;
         }
 
 
@@ -63,8 +76,8 @@ public class InputWrapper : MonoBehaviour
     {
         switch (type)
         {
-            case TYPE_MOBILE:
-                if(Input.touchCount > 0)
+            case TYPE_MOBILE_FIX_ON_TOUCH:
+                if (Input.touchCount > 0)
                 {
                     Touch touch = Input.GetTouch(0);
                     if (pivot.Equals(nanVec2))
@@ -73,7 +86,7 @@ public class InputWrapper : MonoBehaviour
                         stick.GetComponent<SpriteRenderer>().enabled = true;
                         Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
                         pivot = pos;
-                        transform.position = new Vector3(pos.x, pos.y, 0);
+                        transform.position = new Vector3(pivot.x, pivot.y, 0);
 
                         stick.transform.localPosition = new Vector3(0, 0, 0);
                     }
@@ -95,6 +108,69 @@ public class InputWrapper : MonoBehaviour
                     pivot.y = float.NaN;
                     joystickBase.GetComponent<SpriteRenderer>().enabled = false;
                     stick.GetComponent<SpriteRenderer>().enabled = false;
+
+                    transform.position = new Vector3(pivot.x, pivot.y, 0);
+                    movement.x = 0;
+                    movement.y = 0;
+                }
+                break;
+            case TYPE_MOBILE_FIXED_PIVOT:
+                if (Input.touchCount > 0)
+                {
+                    Touch touch = Input.GetTouch(0);
+                    Vector3 tPos = Camera.main.ScreenToWorldPoint(touch.position);
+                    movement.x = tPos.x - pivot.x;
+                    movement.y = tPos.y - pivot.y;
+                    if (movement.magnitude > 0.2f)
+                    {
+                        movement = movement.normalized * 0.2f;
+                    }
+                    stick.transform.localPosition = new Vector3(movement.x, movement.y, 0);
+                }
+                else
+                {
+                    movement.x = 0;
+                    movement.y = 0;
+                }
+                break;
+            case TYPE_MOBILE_FLOATING_PIVOT:
+                if (Input.touchCount > 0)
+                {
+                    Touch touch = Input.GetTouch(0);
+                    if (pivot.Equals(nanVec2))
+                    {
+                        joystickBase.GetComponent<SpriteRenderer>().enabled = true;
+                        stick.GetComponent<SpriteRenderer>().enabled = true;
+                        Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
+                        pivot = pos;
+                        transform.position = new Vector3(pivot.x, pivot.y, 0);
+
+                        stick.transform.localPosition = new Vector3(0, 0, 0);
+                    }
+                    else
+                    {
+                        Vector3 tPos = Camera.main.ScreenToWorldPoint(touch.position);
+                        movement.x = tPos.x - pivot.x;
+                        movement.y = tPos.y - pivot.y;
+                        if (movement.magnitude > 0.2f)
+                        {
+                            movement = movement.normalized * 0.2f;
+                        }
+                        stick.transform.localPosition = new Vector3(movement.x, movement.y, 0);
+                        // If drag too far, the pivot will move relatively.
+                        pivot.x = tPos.x - movement.x;
+                        pivot.y = tPos.y - movement.y;
+                        transform.position = new Vector3(pivot.x, pivot.y, 0);
+                    }
+                }
+                else
+                {
+                    pivot.x = float.NaN;
+                    pivot.y = float.NaN;
+                    joystickBase.GetComponent<SpriteRenderer>().enabled = false;
+                    stick.GetComponent<SpriteRenderer>().enabled = false;
+
+                    transform.position = new Vector3(pivot.x, pivot.y, 0);
                     movement.x = 0;
                     movement.y = 0;
                 }
